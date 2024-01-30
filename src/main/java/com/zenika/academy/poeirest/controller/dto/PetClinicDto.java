76a2f304999c2b9cbef3ac2c2ddb4582263684fe
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-30 11:58:46 +0100

    :recycle: adding the constructor + method String

--- src/main/java/com/zenika/academy/poeirest/controller/dto/PetClinicDto.java
+++ src/main/java/com/zenika/academy/poeirest/controller/dto/PetClinicDto.java
@@ -2,7 +2,16 @@ package com.zenika.academy.poeirest.controller.dto;
 
 public class PetClinicDto {
 
-    private String name;
+    public String name;
+
+    private int id;
+
+    public PetClinicDto(String name) {
+        this.name = name;
+    }
+    public PetClinicDto() {}
+
+    public int getId() { return id; }
 
     public String getName() {
         return name;
@@ -11,4 +20,13 @@ public class PetClinicDto {
     public void setName(String name) {
         this.name = name;
     }
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Override
+    public String toString() {
+        return name ;
+    }
+
+
 }
